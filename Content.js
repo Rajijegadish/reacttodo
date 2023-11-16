d891b128de564472ecb87a92fdf1e66f6a31282a
Rajijegadish <147642252+Rajijegadish@users.noreply.github.com>
2023-11-16 12:10:39 +0530

    Update Content.js

--- Content.js
+++ Content.js
@@ -25,11 +25,15 @@ const Content = () =>{
       const listItems= items.map((item)=> 
       item.id===id ?{...item,checked: !item.checked} :item)
       setItems(listItems)
+      localStorage.setItem("todo_list",JSON.stringify  
+      (listItems))
     }
     const handleDelete=(id)=>{
       const listItems=items.filter((item)=>
       item.id !==id )
       setItems(listItems)
+      localStorage.setItem("todo_list",JSON.stringify  
+      (listItems))
       }
 
     
